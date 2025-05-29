f5ca3b9dd901ef7f95ff32632abae5511795520a
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-05-29 11:33:31 +0200

    Exercise 3 added and modified

---
+++ Brayn Meyer - JavaScript - Exercise 3.js
@@ -0,0 +1,12 @@
+// TODO: Create an empty array called colors
+let colors = []
+// TODO: Add three colors to the array using push()
+colors.push("blue", "yellow", "green")
+// TODO: Create another array called numbers with five numbers
+let numbers = [1, 5, 2, 3, 4]
+// TODO: Remove the last color from the colors array
+colors.splice(2, 1)
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("purple")
+// TODO: Print the length of both arrays
+console.log("Colors : " + colors.length + " " + "Numbers : " + numbers.length)
