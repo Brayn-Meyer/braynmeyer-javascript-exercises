c3e8e82b39bd9cabcf0768a575497a23d7dbf481
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-05-29 11:42:45 +0200

    Exercise 4 added and modified

---
+++ Brayn Meyer - JavaScript - Exercise 4.js
@@ -0,0 +1,15 @@
+// TODO: Create a variable called score and give it a value between 0 and 100
+const score = 100
+// TODO: Create a variable called passingGrade and set it to 60
+const passingGrade = 60
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade
+if (score == 100){
+    console.log("Perfect score. ")
+// TODO: Add an else statement for when the score is below passingGrade
+} else if (score >= passingGrade){
+    console.log("Passed. ")
+// TODO: Add another condition to check if the score is perfect (100)
+} else if (score < passingGrade){
+    console.log("Failed. ")
+}
+// TODO: Print "Passed" or "Failed" based on the score
